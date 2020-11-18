730687f25c96bc240f29a1b4dc14ec8aad6e0ae8
jkyle1234 <skynetJason@gmail.com>
2020-11-17 19:40:42 -0500

    week 2 challenge

---
+++ Week2Challenge.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week2challenge
+{
+    class Week2Challenge
+    {
+        static void Main(string[] args)
+        {
+            Greeter g = new Greeter();
+            g.Hello("bob");
+            g.Farewell("george");
+            g.WhatTimeOfDay(TimeOfDay.Morning);
+            Console.ReadLine();
+        }
+    }
+
+    public enum TimeOfDay { Morning,Afternoon,Night};
+
+    public class Greeter
+    {
+
+        public void Hello(string name)
+        {
+            Console.WriteLine("Hello {0} how are you!!", name);
+        }
+
+        public void Farewell(string message)
+        {
+            Console.WriteLine("Goodbye {0}.", message);
+        }
+
+        public void WhatTimeOfDay(TimeOfDay t)
+        {
+            switch(t)
+            {
+                case TimeOfDay.Morning:
+                    Console.WriteLine("Good morning");
+                    break;
+                case TimeOfDay.Afternoon:
+                    Console.WriteLine("Good Afternoon");
+                    break;
+                case TimeOfDay.Night:
+                    Console.WriteLine("Good evening");
+                    break;
+                default:
+                    Console.WriteLine("Its five o clock somewhere");
+                    break;
+            }
+        }
+
+
+    }
+}
